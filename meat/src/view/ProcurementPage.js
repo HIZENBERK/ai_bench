ec2dcf26bf92c6f4c2e4751375ad5ddc29a2fcca
HIZENBERK <135631332+HIZENBERK@users.noreply.github.com>
2024-06-11 22:06:27 +0900

    클라이언트, 부위 자동 불러오기(미완, 데이터는 받는 거 확인)

--- meat/src/view/ProcurementPage.js
+++ meat/src/view/ProcurementPage.js
@@ -1,44 +1,61 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Pagination from '../component/Pagination'; // Ensure this path is correct
 import '../css/Pagination.css';
 import Datepicker from "../component/DatePicker";
-import {useAuth} from "../component/AuthContext";
+import { useAuth } from "../component/AuthContext";
 import axios from "axios";
+
 const ProcurementPage = () => {
     const [searchResults, setSearchResults] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [resultsPerPage, setResultsPerPage] = useState(10); // Default value is 10
-    const [orderData, setOrderData] = useState([])
     const indexOfLastResult = currentPage * resultsPerPage;
     const indexOfFirstResult = indexOfLastResult - resultsPerPage;
     const currentResults = searchResults.slice(indexOfFirstResult, indexOfLastResult);
+
+    // Part dropdown state
+    const [partOptions, setPartOptions] = useState([]);
+    const [selectedPartOption, setSelectedPartOption] = useState('');
+    const [isPartOpen, setIsPartOpen] = useState(false);
+
+    // client dropdown state
+    const [clientOptions, setclientOptions] = useState([]);
+    const [selectedclientOption, setSelectedclientOption] = useState('');
+    const [isclientOpen, setIsclientOpen] = useState(false);
+
     useEffect(() => {
-        const fetchData = async () => {
-            // try {
-            //     const response = await axios.get('http://localhost:8000/api/ClientInfo/');
-            //     console.log(response.data);
-            //     setClientData(response.data);
-            // } catch (error) {
-            //     console.error('데이터 가져오기 에러:', error);
-            // }
-            // try {
-            //     const response = await axios.get('http://localhost:8000/api/MeatPartInfo/');
-            //     console.log(response.data);
-            //     setMeatPartData(response.data);
-            // } catch (error) {
-            //     console.error('데이터 가져오기 에러:', error);
-            // }
+        const fetchSearchResults = async () => {
             try {
                 const response = await axios.get('http://localhost:8000/api/order/');
                 console.log(response.data);
-                setOrderData(response.data);
                 setSearchResults(response.data);
             } catch (error) {
                 console.error('데이터 가져오기 에러:', error);
             }
-        }
-        fetchData().then(r => null);
+        };
+        fetchSearchResults();
     }, []);
+
+    const fetchPartOptions = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/MeatPartInfo/'); // Replace with actual URL for parts
+            console.log(response.data);
+            setPartOptions(response.data);
+        } catch (error) {
+            console.error('Error fetching part data:', error);
+        }
+    };
+
+    const fetchclientOptions = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/ClientInfo/'); // Replace with actual URL for clients
+            console.log(response.data);
+            setclientOptions(response.data.ClientName);
+        } catch (error) {
+            console.error('Error fetching client data:', error);
+        }
+    };
+
     const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
@@ -49,16 +66,38 @@ const ProcurementPage = () => {
     };
 
     const handleRegisterNavigation = () => {
-        // Add functionality here if needed
         console.log('Register button clicked');
     };
+
     const { authState } = useAuth();
+    let empNo = 'admin';
+    try {
+        empNo = authState.empNo;
+    } catch (e) {}
 
-    let empNo;
-    try {empNo = authState.empNo;}
-    catch (e) {empNo = 'admin';}
+    const handleDropdownClickPart = () => {
+        if (!isPartOpen) {
+            fetchPartOptions();
+        }
+        setIsPartOpen(!isPartOpen);
+    };
 
+    const handleDropdownClickclient = () => {
+        if (!isclientOpen) {
+            fetchclientOptions();
+        }
+        setIsclientOpen(!isclientOpen);
+    };
 
+    const handlePartOptionClick = (option) => {
+        setSelectedPartOption(option);
+        setIsPartOpen(false);
+    };
+
+    const handleclientOptionClick = (option) => {
+        setSelectedclientOption(option);
+        setIsclientOpen(false);
+    };
 
     return (
         <div>
@@ -66,27 +105,43 @@ const ProcurementPage = () => {
                 <h2>발주 등록 페이지</h2>
                 <div className="input-container">
                     <label htmlFor="orderDateTime">발주일시</label>
-                    <Datepicker id="orderDateTime"/>
+                    <Datepicker id="orderDateTime" />
                     <label htmlFor="totalQuantity">발주중량</label>
-                    <input type="text" id="totalQuantity"/>
+                    <input type="text" id="totalQuantity" />
                 </div>
                 <div className="input-container">
                     <label htmlFor="ordererID">발주자(사번)명</label>
-                    {/*<span>{authState.empNo}</span>*/}
-                    <text id="ordererID" >{empNo}</text>
-                    {/*<input type="text" id="ordererID" defaultValue=/>*/}
+                    <text id="ordererID">{empNo}</text>
                     <label htmlFor="expectedDeliveryDate">입고 예정일</label>
-                    <Datepicker id="expectedDeliveryDate"/>
+                    <Datepicker id="expectedDeliveryDate" />
                 </div>
                 <div className="input-container">
                     <label htmlFor="part">부위</label>
-                    <input type="text" id="part"/>
-                    <label htmlFor="supplier">거래처</label>
-                    <input type="text" id="supplier"/>
+                    <input type="text" id="part" value={selectedPartOption} onClick={handleDropdownClickPart} readOnly />
+                    {isPartOpen && (
+                        <ul>
+                            {partOptions.map((option, index) => (
+                                <li key={index} onClick={() => handlePartOptionClick(option)}>
+                                    {option}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                    <label htmlFor="client">거래처</label>
+                    <input type="text" id="client" value={selectedclientOption} onClick={handleDropdownClickclient} readOnly />
+                    {isclientOpen && (
+                        <ul>
+                            {clientOptions.map((option, index) => (
+                                <li key={index} onClick={() => handleclientOptionClick(option)}>
+                                    {option}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </div>
                 <div className="input-container">
                     <label htmlFor="orderAmount">발주금액</label>
-                    <input type="text" id="orderAmount"/>
+                    <input type="text" id="orderAmount" />
                 </div>
                 <button onClick={handleRegisterNavigation} className="register-button">등록</button>
             </div>
@@ -101,23 +156,23 @@ const ProcurementPage = () => {
                 </div>
                 <div className="input-container">
                     <label htmlFor="orderDateTimeSearch">컬럼별 조회 목록</label>
-                    <input type="text" id="orderDateTimeSearch"/>
+                    <input type="text" id="orderDateTimeSearch" />
                     <button>조회</button>
                 </div>
                 <table className="results-table">
                     <thead>
-                        <tr>
-                            <th>순번</th>
-                            <th>발주일시</th>
-                            <th>입고 예정 일</th>
-                            <th>거래처(번호)</th>
-                            <th>발주중량(KG)</th>
-                            <th>부위</th>
-                            <th>발주금액</th>
-                            <th>상태</th>
-                            <th>발주번호</th>
-                            <th>수정</th>
-                        </tr>
+                    <tr>
+                        <th>순번</th>
+                        <th>발주일시</th>
+                        <th>입고 예정 일</th>
+                        <th>거래처(번호)</th>
+                        <th>발주중량(KG)</th>
+                        <th>부위</th>
+                        <th>발주금액</th>
+                        <th>상태</th>
+                        <th>발주번호</th>
+                        <th>수정</th>
+                    </tr>
                     </thead>
                     <tbody>
                     {currentResults.map((result, index) => (
